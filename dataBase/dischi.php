b533bd550f01fcfcd3cca5b8180ab786669d6340
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-23 15:03:51 +0200

    init+dataBase

---
+++ dataBase/dischi.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$dischi = [
+  [
+    'poster' => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    'title' => "New Jersey",
+    'author' => "Bon Jovi",
+    'genre' =>"Rock",
+    'year' => 1988
+  ],
+  [
+    'poster' => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    'title' => "New Jersey",
+    'author' => "Bon Jovi",
+    'genre' =>"Rock",
+    'year' => 1988
+  ],
+  [
+    'poster' => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    'title' => "New Jersey",
+    'author' => "Bon Jovi",
+    'genre' =>"Rock",
+    'year' => 1988
+  ],
+  [
+    'poster' => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    'title' => "New Jersey",
+    'author' => "Bon Jovi",
+    'genre' =>"Rock",
+    'year' => 1988
+  ],
+];
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  include __DIR__.'/dataBase/dischi.php';
+  ?>
+
+  <div class="card">
+    <div class="card__content">
+      <?php
+        foreach($dischi as $disco) {
+          ?>
+            <img src=<?= $disco['poster']?>alt="">
+            <h3 class="album"><?= $disco['title'] ?></h3>
+            <div class="info__album">
+              <div class="author"><?= $disco['author']?></div>
+              <div class="year"><?= $disco['year'] ?></div>
+            </div>
+          <?php
+        }
+      ?>   
+    </div>
+  </div>
+</body>
+</html>
